8d6778991b8ae09a3e49eff48fefb2369fabbf34
death895 <yihen.jin@gmail.com>
2025-03-12 14:45:34 -0400

    Add tap open commands
    made the thing go to google

---
+++ chrome open tab.js
@@ -0,0 +1,5 @@
+//open an about blank
+window.open();
+//other way, fork and change the google.com to something else if you want
+let url = 'https://google.com';
+window.open(url, '_blank').focus();
