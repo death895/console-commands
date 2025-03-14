ac8eac511cc4cd8af4ebd5a177c7bc09f6954266
death895 <yihen.jin@gmail.com>
2025-03-14 09:56:38 -0400

    Update Autoclicks.js

--- commands/Autoclicks.js
+++ commands/Autoclicks.js
@@ -1,5 +1,5 @@
-//will make password later, hold left click to autoclick
-let _button=0;
+//will make password later, hold middle click to autoclick, this is so it works on most games
+let _button=1;
 let _ID;
 document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
 document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
