3c810c0790f8d1024582ef3aa1fef6c0b46d9b92
death895 <yihen.jin@gmail.com>
2025-03-14 10:35:12 -0400

    Update Autoclicks.js

--- commands/Autoclicks.js
+++ commands/Autoclicks.js
@@ -1,5 +1,11 @@
-//will make password later, hold middle click to autoclick, this is so it works on most games
+//hold middle click to autoclick (mouse needed), this is so it works on most games
+let pw = 'Qwfp.1234'
+let input = window.prompt('Please enter the password!')
+if (pw = input) {
 let _button=1;
 let _ID;
 document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
 document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
+} else {
+    window.close()
+}
