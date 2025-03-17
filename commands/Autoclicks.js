814f16b3596c4fe5ac9d99e6dd04585a925b1232
death895 <yihen.jin@gmail.com>
2025-03-17 10:49:49 -0400

    made better pw and activation alert

--- commands/Autoclicks.js
+++ commands/Autoclicks.js
@@ -1,11 +1,12 @@
 //hold middle click to autoclick (mouse needed), this is so it works on most games
-let pw = 'Qwfp.1234'
+let pw = atob('UXdmcC4xMjM0Cg==')
 let input = window.prompt('Please enter the password!')
 if (pw = input) {
 let _button=1;
 let _ID;
 document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
 document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
+window.alert('Autoclicker Enabled')
 } else {
     window.close()
 }
