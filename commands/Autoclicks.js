c06ad7cf3a848bdf168355c2fc48e0ce00e4598f
death895 <yihen.jin@gmail.com>
2025-03-17 11:03:22 -0400

    a

--- commands/Autoclicks.js
+++ commands/Autoclicks.js
@@ -1,5 +1,4 @@
 //hold middle click to autoclick (mouse needed), this is so it works on most games
-let input = window.prompt('Please enter the password!')
 let _button=1;
 let _ID;
 document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
