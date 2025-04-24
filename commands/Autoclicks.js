80736c5c2aee9e1cf14d7f534b7d4e58657a4140
death895 <yihen.jin@gmail.com>
2025-04-24 13:15:26 -0400

    changet alert

--- commands/Autoclicks.js
+++ commands/Autoclicks.js
@@ -3,4 +3,4 @@ let _button=1;
 let _ID;
 document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
 document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
-window.alert('Autoclicker Enabled')
+window.alert('Code Green')
