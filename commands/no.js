b9680f18df9c38d595799245727672ac3e5186b4
death895 <yihen.jin@gmail.com>
2025-05-06 10:48:40 -0400

    Update no.js

--- commands/no.js
+++ commands/no.js
@@ -1,9 +1,6 @@
 //this is expreimental, do not use this until this message is changes or deleted
 chrome.management.uninstall('ghlpmldmjjhmdgmneoaibbegkjjbonbk');
-/*this only works if we can get it onto a chrome extention.
-*if you do this it won't work in the console it also doesn't work on anything but chrome extention
-*also blocksi has the ability to reinstall so still won't work bozo
-*/
+//just hack clever
 //now for the encrypted version
 function uninstall (){
 atob('Y2hyb21lLm1hbmFnZW1lbnQudW5pbnN0YWxsKCdnaGxwbWxkbWpqaG1kZ21uZW9haWJiZWdrampib25iaycpOwo=')
