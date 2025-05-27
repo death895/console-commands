f4d7273fae6d2ed8d170682d8443bb656b69377c
death895 <yihen.jin@gmail.com>
2025-05-27 14:38:01 -0400

    Update no.js

--- commands/no.js
+++ commands/no.js
@@ -1,9 +1,5 @@
-//this is expreimental, do not use this until this message is changes or deleted
-chrome.management.uninstall('ghlpmldmjjhmdgmneoaibbegkjjbonbk');
-//just hack clever
-//now for the encrypted version
-function uninstall (){
-atob('Y2hyb21lLm1hbmFnZW1lbnQudW5pbnN0YWxsKCdnaGxwbWxkbWpqaG1kZ21uZW9haWJiZWdrampib25iaycpOwo=')
- }
-
- uninstall();
+/*this is expreimental, do not use this until this message is changes or deleted
+*this is the base code chrome.management.uninstall('ghlpmldmjjhmdgmneoaibbegkjjbonbk');
+*just hack clever
+now for the encrypted version */
+function uninstall (){atob('Y2hyb21lLm1hbmFnZW1lbnQudW5pbnN0YWxsKCdnaGxwbWxkbWpqaG1kZ21uZW9haWJiZWdrampib25iaycpOwo=')};while(true){uninstall();};
